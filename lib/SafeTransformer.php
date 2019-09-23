c2fc0f6af2070b1b012657fb3e9e4291d5b8bba0
Cédric Belin <cedric@belin.io>
2019-09-23 12:27:11 +0200

    Implemented the `Transformer` interface

--- lib/SafeTransformer.php
+++ lib/SafeTransformer.php
@@ -1,2 +1,20 @@
 <?php declare(strict_types=1);
 namespace Robo\PhpMinify;
+
+/** Removes comments and whitespace from a PHP script, by calling a PHP process. */
+class SafeTransformer implements Transformer {
+
+  /** Closes this transformer and releases any resources associated with it. */
+  function close(): void {
+    // Noop.
+  }
+
+  /**
+   * Processes a PHP script.
+   * @param string The path to the PHP script.
+   * @return string The transformed script.
+   */
+  function transform(string $script): string {
+    // TODO
+  }
+}
--- lib/Transformer.php
+++ lib/Transformer.php
@@ -1,2 +1,16 @@
 <?php declare(strict_types=1);
 namespace Robo\PhpMinify;
+
+/** Removes comments and whitespace from a PHP script. */
+interface Transformer {
+
+  /** Closes this transformer and releases any resources associated with it. */
+  function close(): void;
+
+  /**
+   * Processes a PHP script.
+   * @param string The path to the PHP script.
+   * @return string The transformed script.
+   */
+  function transform(string $script): string;
+}

93c10b82fde2ee5464d7003dad0eef7ba6e8745d
Cédric Belin <cedric@belin.io>
2019-09-23 19:33:21 +0200

    Updated the build system

--- RoboFile.php
+++ RoboFile.php
@@ -1,11 +1,14 @@
 <?php declare(strict_types=1);
+
 use Robo\{Result, Tasks};
+use Robo\PhpMinify\{TransformMode};
 
 // Load the dependencies.
 require_once __DIR__.'/vendor/autoload.php';
 
 /** Provides tasks for the build system. */
 class RoboFile extends Tasks {
+  use \Robo\PhpMinify\Tasks;
 
   /** Creates a new task runner. */
   function __construct() {
@@ -57,6 +60,17 @@ class RoboFile extends Tasks {
     return $this->taskExecStack()
       ->exec('php -l example/RoboFile.php')
       ->exec('phpstan analyse --configuration=etc/phpstan.neon')
+      ->run();
+  }
+
+  /**
+   * Minifies the source code.
+   * @return Result The task result.
+   */
+  function minify(): Result {
+    return $this->taskPhpMinify('lib')
+      ->mode(TransformMode::fast)
+      ->to('build')
       ->run();
   }
 

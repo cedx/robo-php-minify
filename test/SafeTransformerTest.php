8a4a552d4fe120eb70874ddd307d10a0c7af9984
Cédric Belin <cedric@belin.io>
2019-09-23 17:56:09 +0200

    Added the unit tests of the safe transformer

---
+++ test/SafeTransformerTest.php
@@ -0,0 +1,47 @@
+<?php declare(strict_types=1);
+namespace Robo\PhpMinify;
+
+use function PHPUnit\Expect\{expect, it};
+use PHPUnit\Framework\{TestCase};
+
+/** Tests the features of the `Robo\PhpMinify\SafeTransformer` class. */
+class SafeTransformerTest extends TestCase {
+
+  /** @test SafeTransformer->close() */
+  function testClose(): void {
+    $transformer = new SafeTransformer;
+
+    it('should complete without any error', function() use ($transformer) {
+      expect(function() use ($transformer) { $transformer->close(); })->to->not->throw;
+    });
+
+    it('should be callable multiple times', function() use ($transformer) {
+      expect(function() use ($transformer) { $transformer->close(); })->to->not->throw;
+      expect(function() use ($transformer) { $transformer->close(); })->to->not->throw;
+    });
+  }
+
+  /** @test SafeTransformer->transform() */
+  function testTransform(): void {
+    $script = 'test/fixtures/sample.php';
+    $transformer = new SafeTransformer;
+
+    it('should remove the inline comments', function() use ($script, $transformer) {
+      expect($transformer->transform($script))->to->contain("<?= 'Hello World!' ?>");
+    });
+
+    it('should remove the multi-line comments', function() use ($script, $transformer) {
+      expect($transformer->transform($script))->to->contain('namespace dummy; class Dummy');
+    });
+
+    it('should remove the single-line comments', function() use ($script, $transformer) {
+      expect($transformer->transform($script))->to->contain('$className = get_class($this); return $className;');
+    });
+
+    it('should remove the whitespace', function() use ($script, $transformer) {
+      expect($transformer->transform($script))->to->contain('__construct() { }');
+    });
+
+    $transformer->close();
+  }
+}

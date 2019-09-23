06531afcbb1099e431801480d70944a9e5ed2a11
Cédric Belin <cedric@belin.io>
2019-09-23 15:27:06 +0200

    Implemented the safe transformer

--- lib/SafeTransformer.php
+++ lib/SafeTransformer.php
@@ -1,8 +1,21 @@
 <?php declare(strict_types=1);
 namespace Robo\PhpMinify;
 
+use Webmozart\PathUtil\{Path};
+
 /** Removes comments and whitespace from a PHP script, by calling a PHP process. */
 class SafeTransformer implements Transformer {
+
+  /** @var string The path to the PHP executable. */
+  private $executable;
+
+  /**
+   * Creates a new safe transformer.
+   * @param string $executable The path to the PHP executable.
+   */
+  function __construct(string $executable = 'php') {
+    $this->executable = str_replace('/', DIRECTORY_SEPARATOR, Path::canonicalize($executable));
+  }
 
   /** Closes this transformer and releases any resources associated with it. */
   function close(): void {
@@ -15,6 +28,9 @@ class SafeTransformer implements Transformer {
    * @return string The transformed script.
    */
   function transform(string $script): string {
-    // TODO
+    $phpExecutable = escapeshellarg($this->executable);
+    $phpScript = escapeshellarg($script);
+    exec("$phpExecutable -w $phpScript", $output);
+    return implode(PHP_EOL, $output);
   }
 }

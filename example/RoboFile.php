ea75f6bdefcc9bfe69bc49b5db1521ab27c4759f
Cédric Belin <cedric@belin.io>
2019-09-23 19:33:30 +0200

    Updated the example code

--- example/RoboFile.php
+++ example/RoboFile.php
@@ -1,5 +1,7 @@
 <?php declare(strict_types=1);
+
 use Robo\{Result, Tasks};
+use Robo\PhpMinify\{TransformMode};
 
 // Load the dependencies.
 require_once __DIR__.'/vendor/autoload.php';
@@ -12,8 +14,12 @@ class RoboFile extends Tasks {
    * Compresses a given set of PHP scripts.
    * @return Result The task result.
    */
-  function minifyPhp(): Result {
+  function compressPhp(): Result {
+    $isWindows = PHP_OS_FAMILY == 'Windows';
     return $this->taskPhpMinify('path/to/src/**/*.php')
+      ->binary($isWindows ? 'C:\\Program Files\\PHP\\php.exe' : '/usr/bin/php')
+      ->mode($isWindows ? TransformMode::safe : TransformMode::fast)
+      ->silent(stream_isatty(STDOUT))
       ->to('path/to/out')
       ->run();
   }

489efb452cc0679efb800ce2e5c513607b36dbb3
Cédric Belin <cedric@belin.io>
2019-09-26 12:31:02 +0200

    Fixed the CI

--- test/FastTransformerTest.php
+++ test/FastTransformerTest.php
@@ -4,10 +4,7 @@ namespace Robo\PhpMinify;
 use function PHPUnit\Expect\{expect, it};
 use PHPUnit\Framework\{TestCase};
 
-/**
- * Tests the features of the `Robo\PhpMinify\FastTransformer` class.
- * @runTestsInSeparateProcesses
- */
+/** Tests the features of the `Robo\PhpMinify\FastTransformer` class. */
 class FastTransformerTest extends TestCase {
 
   /** @test FastTransformer->close() */
--- test/MinifierTest.php
+++ test/MinifierTest.php
@@ -4,10 +4,7 @@ namespace Robo\PhpMinify;
 use function PHPUnit\Expect\{expect, it};
 use PHPUnit\Framework\{TestCase};
 
-/**
- * Tests the features of the `Robo\PhpMinify\Minifier` class.
- * @runTestsInSeparateProcesses
- */
+/** Tests the features of the `Robo\PhpMinify\Minifier` class. */
 class MinifierTest extends TestCase {
 
   /** @test Minifier->run() */

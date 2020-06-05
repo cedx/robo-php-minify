7ff978da29d6027547c9208a829943a585005df2
Cédric Belin <cedric@belin.io>
2020-06-05 18:45:59 +0200

    Updated the build system

--- tool/lint.ps1
+++ tool/lint.ps1
@@ -3,4 +3,4 @@ Set-StrictMode -Version Latest
 Set-Location (Split-Path $PSScriptRoot)
 
 php -l example/RoboFile.php
-vendor/bin/phpstan analyse --configuration=etc/phpstan.neon
+composer run-script lint

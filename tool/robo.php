4f7b9366a7b9e277d006c64ba4d5d0fad7526034
Cédric Belin <cedric@belin.io>
2020-06-08 17:20:44 +0200

    Added new Composer scripts

---
+++ tool/robo.php
@@ -0,0 +1,24 @@
+<?php declare(strict_types=1);
+
+use Robo\{Result, Tasks};
+use Robo\PhpMinify\{TransformMode};
+
+// Load the dependencies.
+require_once __DIR__."/../vendor/autoload.php";
+
+/** Provides tasks for the build system. */
+class Robo extends Tasks {
+	use \Robo\PhpMinify\Tasks;
+
+	/**
+	 * Builds the project.
+	 * @return Result The task result.
+	 */
+	function build(): Result {
+		return $this->taskPhpMinify("../src")
+			->mode(TransformMode::fast)
+			->silent()
+			->to("../build")
+			->run();
+	}
+}
